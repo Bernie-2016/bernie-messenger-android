9852478236fb8b66342bdec4bef0496c3062ce50
Atticus White <contact@atticuswhite.com>
2016-05-14 15:46:17 -0400

    use remote endpoint for assignments

--- app/utils/assignments.js
+++ app/utils/assignments.js
@@ -1,14 +1,6 @@
-import mock from '../../mock/assignments.json';
-
-// const BASE_URL = 'https://organize.berniesanders.com';
+const BASE_URL = 'https://organize.berniesanders.com';
 
 export function getAssignments () {
-  return new Promise(resolve => {
-    setTimeout(() => resolve(mock), 1000);
-  });
-  // @TODO
-  /*
   return fetch(`${BASE_URL}/contact-assignments.json`)
   .then(response => response.json());
-  */
 }
