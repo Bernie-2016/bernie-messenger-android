9a2fde098bd661eb702c8e439c4d6906b88d9e28
Jacob LeGrone <jacob@jacoblegrone.com>
2016-05-17 20:10:00 -0400

    Add platform query param

--- app/utils/assignments.js
+++ app/utils/assignments.js
@@ -1,6 +1,6 @@
 const BASE_URL = 'https://organize.berniesanders.com';
 
 export function getAssignments () {
-  return fetch(`${BASE_URL}/contact-assignments.json`)
+  return fetch(`${BASE_URL}/contact-assignments.json?platform=android`)
   .then(response => response.json());
 }
